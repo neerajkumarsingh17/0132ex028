e36cd59d486073c9420270819dbe9c6cc3440eca
Neeraj Kumar <47746422+neerajkumarsingh17@users.noreply.github.com>
2020-11-19 17:09:52 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,6 @@
 
 #shortest code in min line
-
+# method 1
 def fibonacci(length, first=0, second=1):
     
     for _ in range(length):
@@ -8,3 +8,6 @@ def fibonacci(length, first=0, second=1):
         nth_num = first + second
         first = second
         second = nth_num
+        
+# method 1
+#comming soon
