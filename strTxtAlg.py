4cc191b2bc35abd9bee431946d90a53cb38d962d
Neeraj Kumar <47746422+neerajkumarsingh17@users.noreply.github.com>
2020-11-25 12:54:17 +0530

    Create strTxtAlg.py

---
+++ strTxtAlg.py
@@ -0,0 +1,54 @@
+#In Python, a string of text can be aligned left, right and center.
+#input = 5
+#o/p =   
+#    H    
+#   HHH   
+#   HHHHH  
+#  HHHHHHH 
+# HHHHHHHHH
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHHHHHHHHHHHHHHHHHHHHHH   
+#   HHHHHHHHHHHHHHHHHHHHHHHHH   
+#   HHHHHHHHHHHHHHHHHHHHHHHHH   
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#   HHHHH               HHHHH             
+#                     HHHHHHHHH 
+#                      HHHHHHH  
+#                       HHHHH   
+#                        HHH    
+#                         H 
+
+
+
+if __name__ == '__main__':
+    thickness = int(input()) #This must be an odd number
+    c = 'H'
+
+    #Top Cone
+    for i in range(thickness):
+        print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+    #Top Pillars
+    for i in range(thickness+1):
+        print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+    #Middle Belt
+    for i in range((thickness+1)//2):
+        print((c*thickness*5).center(thickness*6))    
+
+    #Bottom Pillars
+    for i in range(thickness+1):
+        print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+    #Bottom Cone
+    for i in range(thickness):
+        print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
